746adab2b94ea21f2628ada37578505996a96b4d
Basavaraj L K <lbasavaraj@zeomega.com>
2022-01-24 17:54:49 +0530

    Movie Library

---
+++ Mean_phase-4/Testing_Movie_libraray/spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"The Vilan",
+        rating:"4.5",
+        genres:"Drama",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"Kgf",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"Vikrant Rona",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"Kalpana",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
